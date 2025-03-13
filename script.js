07d32f755e68d9ae9ca3dc359b49ba5faec0f4a7
imanimonae1 <imsappington@cps.edu>
2025-03-13 16:26:05 +0000

    2

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let age= document.querySelector(".age");
 let tribe=document.querySelector(".tribe");
 let quiz=document.querySelector(".quiz");
 
+
 quiz.onclick=function(){
     console.log("clicked")
-}
+    age= Number (age.value);
+    console.log (age)
+    console.log(4);
+
+    tribe=tribe.value;
+    console.log(tribe);
+
+
+
+    if(age===18){
+        console.log("ok 18")
+    }
+};
+
+
