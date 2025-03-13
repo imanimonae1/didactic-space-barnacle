7139dd3fe651db825e7caaf9dc329d975a3c4c60
imanimonae1 <imsappington@cps.edu>
2025-03-13 15:33:48 +0000

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello")
+
+let age= document.querySelector(".age");
+let tribe=document.querySelector(".tribe");
+let quiz=document.querySelector(".quiz");
+
+quiz.onclick=function(){
+    console.log("clicked")
+}
