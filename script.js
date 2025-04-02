2db2ea7d4c57e87d9b758d51c1fc3db4413a90f5
imanimonae1 <imsappington@cps.edu>
2025-04-02 16:27:52 +0000

    5

--- script.js
+++ script.js
@@ -2,32 +2,51 @@ console.log("hello")
 
 let age= document.querySelector(".Age");
 let tribe=document.querySelector(".tribe");
-let quiz=document.querySelector(".quiz");
+let quiz = document.querySelector(".quiz");
+let avatar=document.querySelector(".avatar");
+let kiri=document.querySelector(".kiri");
+kiri.style.display="none";
+let message=document.querySelector(".message");
+let neteyam= document.querySelector(".neteyam");
+neteyam.style.display="none";
+let tsireya= document.querySelector(".tsireya");
+tsireya.style.display="none";
+
+quiz.addEventListener('click',function(){
+    console.log("clicked");
 
 
-
-
-quiz.onclick=function(){
-    console.log("clicked")
     age= Number (age.value);
-    console.log (age)
+    console.log (age);
     console.log(4);
 
     tribe=tribe.value;
     
-if(age<=15 && tribe=== "Sully Family"){
+if(age<=15 && tribe==="Sully Family"){
 console.log(tribe);
+avatar.style.display="none";
+kiri.style.display="block";
+tsireya.style.display="none";
+message.innerHTML="YOU ARE KIRI!"
 }
 
-else(age>=16 && tribe=== "Sully Family")
-console.log(tribe)
-
+else if(age>=16 && tribe=== "Sully Family"){
+console.log(tribe);
+kiri.style.display="none";
+avatar.style.display="none";
+neteyam.style.display="block";
+tsireya.style.display="none";
+message.innerHTML="YOU ARE NETEYAM";
+}
 
     if(age>=16 && tribe==="Ocean Clan"){
        
     }
-    else( age<15 && tribe==="Ocean Clan")
-    console.log(tribe);
-};
-
+    else if( age<15 && tribe==="Ocean Clan"){
+        tsireya.style.display="block";
+        avatar.style.display="none";
+        kiri.style.display="none";
+        message.innerHTML="YOU ARE TSIREYA";
+    }
+});
 
